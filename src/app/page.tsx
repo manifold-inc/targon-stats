9fc724c70801d6837b558fd29808f6f50084642b
darwich6 <adarwich2010@gmail.com>
2024-09-04 12:37:27 -0500

    fix: split query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Page({ searchParams }: { searchParams: { verified?
         .filter(Boolean) as string[];
     }
 
-    const stats = await db
+    const innerAvg = db
         .select({
           minute:
             sql<string>`DATE_FORMAT(${ValidatorRequest.timestamp}, '%Y-%m-%d %H:%i:00')`.mapWith(
@@ -37,26 +37,25 @@ export default async function Page({ searchParams }: { searchParams: { verified?
                 );
                 return utc;
               },
-            ),
+            ).as('minute'),
           avg_jaro: sql<number>`
             AVG(
               (SELECT AVG(CAST(jt.value AS DECIMAL))
               FROM JSON_TABLE(${MinerResponse.stats}->'$.jaros', '$[*]' COLUMNS (value DOUBLE PATH '$')) AS jt)
-              )`.mapWith(Number),
+              )`.mapWith(Number).as('avg_jaro'),
           avg_wps:
             sql<number>`AVG(CAST(${MinerResponse.stats}->'$.wps' AS DECIMAL))`.mapWith(
               Number,
-            ),
+            ).as('avg_wps'),
           avg_total_time:
             sql<number>`AVG(CAST(${MinerResponse.stats}->'$.total_time' AS DECIMAL(8,5)))`.mapWith(
               Number,
-            ),
+            ).as('avg_total_time'),
           avg_time_to_first_token:
             sql<number>`AVG(CAST(${MinerResponse.stats}->'$.time_to_first_token' AS DECIMAL(8,5)))`.mapWith(
               Number,
-            ),
-          valiName: sql<string | undefined> `IFNULL(${Validator.valiName}, '')`,
-        })
+            ).as('avg_time_to_first_token'),
+          valiName: sql<string | undefined> `IFNULL(${Validator.valiName}, '')`.as('valiName'),})
         .from(MinerResponse)
         .innerJoin(
           ValidatorRequest,
@@ -81,10 +80,14 @@ export default async function Page({ searchParams }: { searchParams: { verified?
           sql`DATE_FORMAT(${ValidatorRequest.timestamp}, '%Y-%m-%d %H:%i:00')`,
           Validator.valiName,
         )
-        .orderBy(
-          sql`DATE_FORMAT(${ValidatorRequest.timestamp}, '%Y-%m-%d %H:%i:00')`,
-        );
-      return <ClientPage data={stats} initialVerified={verified} initialValidators={activeValidators} />;
+        .as("innerAvg");
+      
+      const orderedStats = await db
+      .select()
+      .from(innerAvg)
+      .orderBy(innerAvg.minute)
+
+      return <ClientPage data={orderedStats} initialVerified={verified} initialValidators={activeValidators} />;
   } catch (error) {
     console.error("Error fetching stats:", error);
     return <div> Error fetching stats...</div>
