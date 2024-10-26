27b752350c328fa6a3f6386655c2d61abf52c92e
darwich6 <adarwich2010@gmail.com>
2024-10-26 11:22:05 -0500

    fix: only show validators on their path

--- src/app/_components/ClientHeader.tsx
+++ src/app/_components/ClientHeader.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
 
 import { useAuth } from "./providers";
@@ -14,6 +14,8 @@ const ClientHeader = ({ validators }: ClientHeaderProps) => {
   const auth = useAuth();
   const router = useRouter();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const showValidatorRequests = pathname === "/" || pathname === "/stats/miner";
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [selectedBits, setSelectedBits] = useState<string | null>(null);
@@ -72,40 +74,45 @@ const ClientHeader = ({ validators }: ClientHeaderProps) => {
         <Link href="/stats/miner">Miners</Link>
         <Link href="/stats/validator">Validators</Link>
         <Link href="/stats/overview">Historical</Link>
-        <div className="relative" ref={dropdownRef}>
-          <button onClick={toggleDropdown} className="flex items-center gap-1">
-            Validator Requests
-            {isDropdownOpen ? (
-              <ChevronUp className="px-1 py-0.5" />
-            ) : (
-              <ChevronDown className="px-1 py-0.5" />
-            )}
-          </button>
-          {isDropdownOpen && validators && (
-            <div
-              className={`-translate-x-1/5 absolute right-0 z-50 mt-2 min-w-fit transform whitespace-nowrap rounded-md bg-white p-2 shadow-lg dark:bg-neutral-700 dark:text-gray-300`}
+        {showValidatorRequests && (
+          <div className="relative" ref={dropdownRef}>
+            <button
+              onClick={toggleDropdown}
+              className="flex items-center gap-1"
             >
-              {validators.length > 0 ? (
-                validators.map((validator, index) => (
-                  <div
-                    key={index}
-                    onClick={() => toggleValidator(index)}
-                    className="flex cursor-pointer items-center gap-2 p-2"
-                  >
-                    <span className="flex h-4 w-4 items-center justify-center rounded-sm border border-gray-400">
-                      {(selectedBits?.[index] ?? "1") == "1" ? (
-                        <Check className="text-black dark:text-white" />
-                      ) : null}
-                    </span>
-                    {validator}
-                  </div>
-                ))
+              Validator Requests
+              {isDropdownOpen ? (
+                <ChevronUp className="px-1 py-0.5" />
               ) : (
-                <div>No validators available</div>
+                <ChevronDown className="px-1 py-0.5" />
               )}
-            </div>
-          )}
-        </div>
+            </button>
+            {isDropdownOpen && validators && (
+              <div
+                className={`-translate-x-1/5 absolute right-0 z-50 mt-2 min-w-fit transform whitespace-nowrap rounded-md bg-white p-2 shadow-lg dark:bg-neutral-700 dark:text-gray-300`}
+              >
+                {validators.length > 0 ? (
+                  validators.map((validator, index) => (
+                    <div
+                      key={index}
+                      onClick={() => toggleValidator(index)}
+                      className="flex cursor-pointer items-center gap-2 p-2"
+                    >
+                      <span className="flex h-4 w-4 items-center justify-center rounded-sm border border-gray-400">
+                        {(selectedBits?.[index] ?? "1") == "1" ? (
+                          <Check className="text-black dark:text-white" />
+                        ) : null}
+                      </span>
+                      {validator}
+                    </div>
+                  ))
+                ) : (
+                  <div>No validators available</div>
+                )}
+              </div>
+            )}
+          </div>
+        )}
         {auth.status === "AUTHED" ? (
           <>
             <Link href="/docs">API</Link>
