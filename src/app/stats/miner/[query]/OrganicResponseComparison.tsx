719efc43ac8d3f24be9839680993ded7ab5200fa
darwich6 <adarwich2010@gmail.com>
2025-03-14 09:35:01 -0500

    fix: copy the request id rather than hotkey

--- src/app/stats/miner/[query]/OrganicResponseComparison.tsx
+++ src/app/stats/miner/[query]/OrganicResponseComparison.tsx
@@ -116,7 +116,7 @@ const OrganicResponseComparison: React.FC<OrganicResponseComparisonProps> = ({
                         <span>req_{response.th_pub_id}</span>
                         <button
                           className="ml-2 cursor-pointer"
-                          onClick={() => handleCopyClipboard(response.hotkey)}
+                          onClick={() => handleCopyClipboard(response.th_pub_id)}
                         >
                           <Copy className="z-10 h-4 w-4 text-gray-500 dark:text-gray-300" />
                         </button>
