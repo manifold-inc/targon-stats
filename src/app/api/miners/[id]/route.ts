f2f95751de67e349754fc747646946ef0dd7e8d2
Alan Cho <alancho397@gmail.com>
2025-07-14 15:55:27 -0500

    fix: resolve bug on individual query

--- src/app/api/miners/[id]/route.ts
+++ src/app/api/miners/[id]/route.ts
@@ -2,9 +2,13 @@ import { NextResponse } from "next/server";
 
 import { getMiner } from "@/server/api/routers/miners";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = params;
+
   try {
-    const { id } = params;
     if (!id) {
       return NextResponse.json(
         {
