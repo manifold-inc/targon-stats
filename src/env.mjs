e7476f50529d29e5cd88d2ba6f3fd9f323e971bd
josh <hey@imjosh.dev>
2025-07-14 14:17:45 -0500

    fix: remove unused env

--- src/env.mjs
+++ src/env.mjs
@@ -7,25 +7,13 @@ configDotenv({ path: "./../.env" });
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]).optional(),
-    GOOGLE_CLIENT_SECRET: z.string(),
-    GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_REDIRECT_URI: z.string(),
-
     MONGO_URI: z.string(),
-
     VERCEL_URL: z.string(),
-  },
-  client: {
-    NEXT_PUBLIC_HUB_API_ENDPOINT: z.string(),
   },
 
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
     MONGO_URI: process.env.MONGO_URI,
-    NEXT_PUBLIC_HUB_API_ENDPOINT: process.env.NEXT_PUBLIC_HUB_API_ENDPOINT,
     VERCEL_URL: process.env.VERCEL_ENV ?? "http://localhost:3000",
   },
 
