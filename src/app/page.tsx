3313850da680f3b8b719fe14fccc3e8525f8688f
Alan Cho <alancho397@gmail.com>
2025-07-16 08:09:57 -0700

    feat: add tao price (#33)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 
 import BidTable from "@/app/_components/BidTable";
 import CurrentBlock from "@/app/_components/CurrentBlock";
+import EmissionPool from "@/app/_components/EmissionPool";
 import MaxBid from "@/app/_components/MaxBid";
 import MinerTable from "@/app/_components/MinerTable";
 import Search from "@/app/_components/Search";
+import TaoPrice from "@/app/_components/TaoPrice";
 import ToggleTable from "@/app/_components/ToggleTable";
 import { type AuctionState } from "@/server/api/routers/chain";
 import { reactClient } from "@/trpc/react";
@@ -44,6 +46,8 @@ export default function HomePage() {
         <div className="mt-4 flex justify-between">
           <CurrentBlock block={data?.block || 0} />
           <MaxBid maxBid={data?.max_bid || 0} />
+          <TaoPrice price={data?.tao_price || 0} />
+          <EmissionPool pool={data?.emission_pool || 0} />
         </div>
 
         <div className="mt-8">
