1a5dc552394e4dd939101ed6911f6d534a570807
Alan Cho <alancho397@gmail.com>
2025-07-21 12:35:39 -0700

    feat: create endpoint to grab attestation errors based on miner id (#41)
    * feat: add route to grab attestation report
    
    * feat: add epistula verification
    
    * fix: epistula checks
    
    ---------
    
    Co-authored-by: josh <hey@imjosh.dev>

---
+++ src/app/api/miners/attest/error/[id]/route.ts
@@ -0,0 +1,117 @@
+import { NextResponse } from "next/server";
+
+import { connectToMongoDb } from "@/schema/mongoDB";
+import { getEpistulaHeaders, verify } from "@/utils/signature";
+
+interface AttestationReport {
+  failed: Record<string, string>;
+  hotkey_to_uid: Record<string, string>;
+}
+
+async function getAttestationErrors(
+  uid: string,
+): Promise<[AttestationReport, null] | [null, string]> {
+  const mongoDb = await connectToMongoDb();
+  if (!mongoDb) {
+    return [null, "Failed to connect to MongoDB"];
+  }
+
+  const data = await mongoDb
+    .collection("miner_info")
+    .find({}, { projection: { attest_errors: 1, _id: 0, hotkey_to_uid: 1 } })
+    .sort({ block: -1 })
+    .limit(1)
+    .toArray();
+  if (!data[0]) {
+    return [null, "Failed to get attestation report"];
+  }
+
+  const failed = data[0].attest_errors as Record<
+    string,
+    Record<string, string>
+  >;
+
+  const hotkeytouid = data[0].hotkey_to_uid as Record<string, string>;
+
+  const report: AttestationReport = {
+    failed: failed[uid] || {},
+    hotkey_to_uid: hotkeytouid,
+  };
+
+  return [report, null];
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+): Promise<NextResponse> {
+  const { id } = await params;
+  try {
+    if (!id) throw new Error("No id provided");
+    let headers;
+    try {
+      headers = getEpistulaHeaders(request.headers);
+    } catch (e) {
+      console.log(e);
+      return NextResponse.json(
+        {
+          error: "unauthorized",
+        },
+        { status: 401 },
+      );
+    }
+    const { verified, error } = await verify(
+      headers.signedBy,
+      headers.signature,
+      headers.uuid,
+      "",
+      headers.timestamp,
+      headers.signedFor,
+    );
+    if (!verified) {
+      console.log(error);
+      return NextResponse.json(
+        {
+          error: "unauthorized",
+        },
+        { status: 401 },
+      );
+    }
+
+    const [report, err] = await getAttestationErrors(id);
+    if (err || report === null) {
+      console.log(error);
+      return NextResponse.json(
+        {
+          error: "unauthorized",
+        },
+        { status: 401 },
+      );
+    }
+    if (report.hotkey_to_uid[headers.signedBy] !== id) {
+      console.log(error);
+      return NextResponse.json(
+        {
+          error: "unauthorized",
+        },
+        { status: 401 },
+      );
+    }
+    return NextResponse.json(
+      {
+        data: report.failed,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Internal server error";
+    console.log(error);
+    return NextResponse.json(
+      {
+        error: { message, code: "INTERNAL_ERROR", statusCode: 500 },
+      },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/utils/signature.ts
@@ -0,0 +1,61 @@
+import { createHash } from "crypto";
+import Keyring from "@polkadot/keyring";
+import { waitReady } from "@polkadot/wasm-crypto";
+
+interface Verification {
+  verified: boolean;
+  error?: string;
+}
+
+export async function verify(
+  ss58: string,
+  signature: string,
+  uuid: string,
+  body: string,
+  timestamp: string,
+  signedFor: string,
+): Promise<Verification> {
+  const timestampNumber: number = parseInt(timestamp, 10);
+  if (isNaN(timestampNumber)) {
+    return { verified: false, error: "Invalid timestamp" };
+  }
+
+  const requestTimestamp = new Date(timestampNumber * 1000);
+  const now = new Date();
+  const timeDelta = 8000;
+
+  if (now.getTime() - requestTimestamp.getTime() > timeDelta) {
+    return { verified: false, error: "Timestamp is too old" };
+  }
+
+  const bodyHash = createHash("sha256").update(body).digest("hex");
+
+  const message = `${bodyHash}.${uuid}.${timestamp}.${signedFor}`;
+  await waitReady();
+
+  const keyring = new Keyring({ type: "sr25519" });
+  const keypair = keyring.addFromAddress(ss58);
+  const hex = Uint8Array.from(Buffer.from(signature.slice(2), "hex"));
+  const isValid = keypair.verify(message, hex, keypair.publicKey);
+  if (!isValid) return { verified: false, error: "Invalid signature" };
+
+  return { verified: true };
+}
+
+export function getEpistulaHeaders(headers: Headers) {
+  const signature = headers.get("Epistula-Request-Signature");
+  if (!signature) throw new Error("Signature not found");
+
+  const uuid = headers.get("Epistula-Uuid");
+  if (!uuid) throw new Error("UUID not found");
+
+  const timestamp = headers.get("Epistula-Timestamp");
+  if (!timestamp) throw new Error("Timestamp not found");
+
+  const signedFor = headers.get("Epistula-Signed-For");
+
+  const signedBy = headers.get("Epistula-Signed-By");
+  if (!signedBy) throw new Error("Signed by not found");
+
+  return { signature, uuid, timestamp, signedFor: signedFor ?? "", signedBy };
+}
