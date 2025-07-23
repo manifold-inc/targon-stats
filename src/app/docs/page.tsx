363a4211325b6679ab9dae9851ac9a28695f7a1b
Ryandpark2006 <ryandpark2006@gmail.com>
2025-07-23 11:58:13 -0500

    fix: formatting

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Suspense, useState } from "react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import BlockSelector from "@/app/_components/BlockSelector";
